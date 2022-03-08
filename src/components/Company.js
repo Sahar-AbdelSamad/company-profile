041253ec202e545a0fcb629b53078cd5704b92f9
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-08 09:43:56 +0200

    Fetch company information and render UI

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import './Company.css';
 
-const Company = () => (
-  <h1>Company</h1>
-);
+const Company = () => {
+  const company = useSelector((state) => state.companyReducer);
+  return (
+    <>
+      <section className="logo">
+        <img src={company[0].image} alt="company logo" />
+      </section>
+      <section>
+        {Object.entries(company[0]).map(([key, value]) => (
+          <div className="info" key={uuidv4()}>
+            <div className="keys">
+              {key}
+            </div>
+            <div className="Value">
+              {value}
+            </div>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
 
 export default Company;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './redux/configureStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
