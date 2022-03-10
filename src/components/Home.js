d3b0b147cfaedeb262cf3621b9b923a85c88ce57
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-10 19:14:00 +0200

    Style Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,49 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import { fetchData } from '../redux/company/company';
+import nasdaq from '../images/nasdaq.png';
+import dowjones from '../images/dowjones.png';
+import Header from './Header';
 import './Home.css';
 
 const Home = () => {
   const home = useSelector((state) => state.homeReducer);
-  const [selected, setSelected] = useState('Choose a company');
+  const [companyInfo, setCompanyInfo] = useState('');
+  const [selected, setSelected] = useState('');
+  const [search, setSearch] = useState('');
   const dispatch = useDispatch();
+
   const select = (e) => {
-    const str = e.target.value;
-    setSelected(str);
-    const mySubString = str.substring(
-      str.indexOf('(') + 1,
-      str.lastIndexOf(')'),
-    );
-    dispatch(fetchData(mySubString));
+    setCompanyInfo(e.target.value);
+    const symbol = e.target.value.slice(0, 8);
+    setSelected(symbol);
   };
+
+  const filteredData = home.filter((company) => {
+    const data = Object.keys(company).some((key) => {
+      const res = company[key].toLowerCase().includes(selected.toLowerCase())
+      && company[key].toLowerCase().includes(search.toLowerCase());
+      return res;
+    });
+    return data;
+  });
   return (
-    <div>
-      <section className="home">Section1</section>
+    <>
+      <Header />
+      <section className="homeSection1">
+        <ul className="nasdaq">
+          <li>Nasdaq 100</li>
+          <li><img src={nasdaq} alt="nasdaq" /></li>
+        </ul>
+        <ul className="dowjones">
+          <li>Dow Jones</li>
+          <li><img src={dowjones} alt="dowjones" /></li>
+        </ul>
+      </section>
       <section className="search-bar">
-        <select id="company" name="company" value={selected} onChange={select}>
-          <option value="Choose a company" disabled>Choose a company</option>
+        <input type="search" placeholder="Search Company Name or Symbol" value={search} onChange={(e) => setSearch(e.target.value)} />
+        <select id="company" name="company" value={companyInfo} onChange={select}>
+          <option value="">Choose a company</option>
           {Object.entries(home).map(([key, value]) => (
             <option key={key} value={`${value.name} (${value.symbol})`}>
               {`${value.name} (${value.symbol})`}
@@ -30,27 +51,35 @@ const Home = () => {
           ))}
         </select>
       </section>
-      <NavLink to="/Company">{selected}</NavLink>
+
       <section className="cards">
-        {Object.entries(home).map(([key, value]) => (
-          <NavLink key={key} to="/Company">
-            <div
-              role="button"
-              tabIndex={0}
-              onKeyDown={() => dispatch(fetchData(value.symbol))}
-              onClick={() => dispatch(fetchData(value.symbol))}
-            >
-              {value.name}
-              <br />
-              {value.symbol}
-              <br />
-              {value.exchange}
-              <br />
-            </div>
-          </NavLink>
+        {filteredData === '' ? Object.entries(home).map(([key, value]) => (
+          <a onClick={() => dispatch(fetchData(value.symbol))} key={key} href={`/${value.symbol}`}>
+            <ul>
+              <li className="companyName">
+                {value.name}
+                <span style={{ fontWeight: 300 }}>{`   (${value.symbol})`}</span>
+              </li>
+              <li>
+                {value.exchange}
+              </li>
+            </ul>
+          </a>
+        )) : Object.entries(filteredData).map(([key, value]) => (
+          <a onClick={() => dispatch(fetchData(value.symbol))} key={key} href={`/${value.symbol}`}>
+            <ul>
+              <li className="companyName">
+                {value.name}
+                <span style={{ fontWeight: 300 }}>{`   (${value.symbol})`}</span>
+              </li>
+              <li>
+                {value.exchange}
+              </li>
+            </ul>
+          </a>
         ))}
       </section>
-    </div>
+    </>
   );
 };
 
